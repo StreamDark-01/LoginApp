903f41a5b2cb06ee43be2628f75974c01a79ea80
User <arifanafi14@gmail.com>
2022-10-12 14:50:49 +0800

    First App Login

--- app/src/main/java/com/example/loginapp/MainActivity.java
+++ app/src/main/java/com/example/loginapp/MainActivity.java
@@ -2,32 +2,81 @@ package com.example.loginapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText etName;
     Button btnName;
+    EditText etBirth;
+    EditText date;
+    DatePickerDialog datePickerDialog;
+
+
     public final static String Message_key = "com.example.loginapp.message_key";
+    public final static String Birth_key = "com.example.loginapp.Birth_key";
+    public final static String Date_key = "com.example.loginapp.Date_key";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        date = (EditText) findViewById(R.id.etBirth);
+        // perform click event on edit text
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // calender class's instance and get current date , month and year from calender
+                final Calendar c = Calendar.getInstance();
+                int mYear = c.get(Calendar.YEAR); // current year
+                int mMonth = c.get(Calendar.MONTH); // current month
+                int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+                // date picker dialog
+                datePickerDialog = new DatePickerDialog(MainActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // set day of month , month and year value in the edit text
+                                date.setText(dayOfMonth + "/"
+                                        + (monthOfYear + 1) + "/" + year);
+
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+            }
+        });
     }
     public void send_data(View view)
     {
         etName = findViewById(R.id.etName);
         btnName = findViewById(R.id.btnName);
-        String message = etName.getText().toString();
-        Intent intent  = new Intent(MainActivity.this,secondactivity.class);
+        etBirth = findViewById(R.id.etBirth);
+        //date = findViewById( R.id.date);
 
+
+        String message = etName.getText().toString();
+        String birthtxt = etBirth.getText().toString();
+        String dateTxt = date.getText().toString();
+
+
+        Intent intent  = new Intent(MainActivity.this,secondactivity.class);
         intent.putExtra(Message_key,message);
+        intent.putExtra(Birth_key,birthtxt);
+        intent.putExtra(Date_key,dateTxt);
         startActivity(intent);
+
 
 
 
--- app/src/main/java/com/example/loginapp/secondactivity.java
+++ app/src/main/java/com/example/loginapp/secondactivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class secondactivity extends AppCompatActivity {
 
     public final static String Message_key = "com.example.loginapp.message_key";
+    public final static String Birth_key = "com.example.loginapp.Birth_key";
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +21,14 @@ public class secondactivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String message = intent.getStringExtra(Message_key);
+        String birthtxt = intent.getStringExtra(Birth_key);
+
+        ImageView imageView = new ImageView(this);
+
         TextView textView = new TextView(this);
-        textView.setTextSize(45);
-        textView.setText(message);
-        setContentView(textView);
+        textView.setTextSize(20);
+        textView.setText("Name : " +message+ "\nBirthday : "+birthtxt);
+
+        setContentView(imagetextView);
     }
 }
