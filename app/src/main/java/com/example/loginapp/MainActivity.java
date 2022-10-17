d03e944818c07ee2d73c35b07c4fda261f162a73
User <arifanafi14@gmail.com>
2022-10-18 00:34:52 +0800

    Final Labtask 1

--- app/src/main/java/com/example/loginapp/MainActivity.java
+++ app/src/main/java/com/example/loginapp/MainActivity.java
@@ -18,12 +18,16 @@ public class MainActivity extends AppCompatActivity {
     Button btnName;
     EditText etBirth;
     EditText date;
+    EditText etAddress;
+    EditText etNotel;
     DatePickerDialog datePickerDialog;
+
 
 
     public final static String Message_key = "com.example.loginapp.message_key";
     public final static String Birth_key = "com.example.loginapp.Birth_key";
-    public final static String Date_key = "com.example.loginapp.Date_key";
+    public final static String Addre_key = "com.example.loginapp.Addre_key";
+    public final static String NoTel_key = "com.example.loginapp.NoTel_key";
 
 
     @Override
@@ -51,34 +55,34 @@ public class MainActivity extends AppCompatActivity {
                                 // set day of month , month and year value in the edit text
                                 date.setText(dayOfMonth + "/"
                                         + (monthOfYear + 1) + "/" + year);
-
                             }
                         }, mYear, mMonth, mDay);
                 datePickerDialog.show();
             }
         });
     }
+
     public void send_data(View view)
     {
-        etName = findViewById(R.id.etName);
         btnName = findViewById(R.id.btnName);
-        etBirth = findViewById(R.id.etBirth);
-        //date = findViewById( R.id.date);
 
+        etName = findViewById(R.id.etName);
+        etBirth = findViewById(R.id.etBirth);
+        etAddress = findViewById(R.id.etAddress);
+        etNotel = findViewById(R.id.etPhone);
 
         String message = etName.getText().toString();
         String birthtxt = etBirth.getText().toString();
-        String dateTxt = date.getText().toString();
+        String Addretxt = etAddress.getText().toString();
+        String NoTeltxt = etNotel.getText().toString();
 
 
         Intent intent  = new Intent(MainActivity.this,secondactivity.class);
         intent.putExtra(Message_key,message);
         intent.putExtra(Birth_key,birthtxt);
-        intent.putExtra(Date_key,dateTxt);
+        intent.putExtra(Addre_key,Addretxt);
+        intent.putExtra(NoTel_key,NoTeltxt);
         startActivity(intent);
-
-
-
 
     }
 
--- app/src/main/java/com/example/loginapp/secondactivity.java
+++ app/src/main/java/com/example/loginapp/secondactivity.java
@@ -9,26 +9,39 @@ import android.widget.TextView;
 
 public class secondactivity extends AppCompatActivity {
 
+    TextView tvName,tvBirthday,tvNumPhone,tvAddress;
+
     public final static String Message_key = "com.example.loginapp.message_key";
     public final static String Birth_key = "com.example.loginapp.Birth_key";
-
-
-
+    public final static String Addre_key = "com.example.loginapp.Addre_key";
+    public final static String NoTel_key = "com.example.loginapp.NoTel_key";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secondactivity);
 
         Intent intent = getIntent();
-        String message = intent.getStringExtra(Message_key);
-        String birthtxt = intent.getStringExtra(Birth_key);
+        //String message = intent.getStringExtra(Message_key);
+        //String txt1 = intent.getStringExtra(Birth_key);
 
-        ImageView imageView = new ImageView(this);
+        String str = intent.getStringExtra(Message_key);
+        tvName = (TextView) findViewById(R.id.name);
+        tvName.setText(str);
 
-        TextView textView = new TextView(this);
-        textView.setTextSize(20);
-        textView.setText("Name : " +message+ "\nBirthday : "+birthtxt);
+        String Birth = intent.getStringExtra(Birth_key);
+        tvBirthday = (TextView) findViewById(R.id.tvBirthday);
+        tvBirthday.setText(Birth);
 
-        setContentView(imagetextView);
+        String address = intent.getStringExtra(NoTel_key);
+        tvAddress = (TextView)findViewById(R.id.tvPhoneNo);
+        tvAddress.setText(address);
+
+        String numPhone = intent.getStringExtra(Addre_key);
+        tvNumPhone= (TextView)findViewById(R.id.tvAddress);
+        tvNumPhone.setText(numPhone);
+
+
+
     }
-}
+    }
